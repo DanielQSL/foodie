f03f7950cd63087845c595e34ae64f8ea8a9dccc
qianshuailong <qianshuailong@vcredit.com>
2020-01-08 22:44:32 +0800

    feat:Mybatis-Plus配置

---
+++ foodie-api/src/main/java/com/qsl/foodie/config/MybatisPlusConfig.java
@@ -0,0 +1,34 @@
+package com.qsl.foodie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis-Plus配置
+ *
+ * @author DanielQSL
+ */
+@Configuration
+@MapperScan("com.qsl.foodie.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor().setCountSqlParser(new JsqlParserCountOptimize(true));
+    }
+
+    /**
+     * 乐观锁
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+}
